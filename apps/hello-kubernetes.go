272a52b2e9a259db038626194af9467107e948a9
Patrick MARIE <pm@mkz.me>
2022-10-10 22:22:56 +0200

    Adding hello-kubernetes deploy.

--- apps/hello-kubernetes.go
+++ apps/hello-kubernetes.go
@@ -26,5 +26,37 @@ func NewHelloKubernetesChart(scope constructs.Construct) cdk8s.Chart {
 		},
 	)
 
+	labels := map[string]*string{
+		"app.kubernetes.io/name": jsii.String(appName),
+	}
+
+	k8s.NewKubeDeployment(
+		chart,
+		jsii.String("deploy"),
+		&k8s.KubeDeploymentProps{
+			Metadata: &k8s.ObjectMeta{
+				Namespace: jsii.String(appName),
+			},
+			Spec: &k8s.DeploymentSpec{
+				Selector: &k8s.LabelSelector{
+					MatchLabels: &labels,
+				},
+				Template: &k8s.PodTemplateSpec{
+					Metadata: &k8s.ObjectMeta{
+						Labels: &labels,
+					},
+					Spec: &k8s.PodSpec{
+						Containers: &[]*k8s.Container{
+							{
+								Name:  jsii.String(appName),
+								Image: jsii.String("paulbouwer/hello-kubernetes:1.10.1"),
+							},
+						},
+					},
+				},
+			},
+		},
+	)
+
 	return chart
 }
