08df6987f81e8302862bd69a7d3b8e7471b9b7eb
Patrick MARIE <pm@mkz.me>
2022-10-16 22:23:25 +0200

    Using SealedSecrets for Grafana admin account

--- k8s-helpers/objects.go
+++ k8s-helpers/objects.go
@@ -3,9 +3,11 @@ package k8s_helpers
 import (
 	"fmt"
 
+	"git.mkz.me/mycroft/k8s-home/imports/bitnamicom"
 	"git.mkz.me/mycroft/k8s-home/imports/k8s"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
+	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
 )
 
 func NewNamespace(chart constructs.Construct, name string) k8s.KubeNamespace {
@@ -15,6 +17,22 @@ func NewNamespace(chart constructs.Construct, name string) k8s.KubeNamespace {
 		&k8s.KubeNamespaceProps{
 			Metadata: &k8s.ObjectMeta{
 				Name: jsii.String(name),
+			},
+		},
+	)
+}
+
+func NewSealedSecret(chart constructs.Construct, namespace, name string, secrets map[string]*string) bitnamicom.SealedSecret {
+	return bitnamicom.NewSealedSecret(
+		chart,
+		jsii.String("ss"),
+		&bitnamicom.SealedSecretProps{
+			Metadata: &cdk8s.ApiObjectMetadata{
+				Name:      jsii.String(name),
+				Namespace: jsii.String(namespace),
+			},
+			Spec: &bitnamicom.SealedSecretSpec{
+				EncryptedData: &secrets,
 			},
 		},
 	)
