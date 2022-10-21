4e50a953b2e8a5402893ae96181a7f7e4691f9cb
Patrick MARIE <pm@mkz.me>
2022-10-21 23:08:27 +0200

    Use secret for dex configuration

--- apps/dex-idp.go
+++ apps/dex-idp.go
@@ -42,5 +42,17 @@ func NewDexIdpChart(scope constructs.Construct) cdk8s.Chart {
 		nil,
 	)
 
+	// Create configuration
+	// See contrib/extract-dex-config.sh to extract/seal new configuration
+	// This is used in configs/dex-idp.yaml
+	k8s_helpers.NewSealedSecret(
+		chart,
+		namespace,
+		"dex-config",
+		map[string]*string{
+			"config.yaml": jsii.String("AgDIMrmuMJFfWPxdtE89v7ISUKbb3M11VsfqWgEPjN3HdlTg82QXskAA+EVt1D0uAunlsdImaU+8xF2dvO+Ei4sED34HvIQJe9AD6dcNp8uVrsMM3TUa2K8tS8LVT9XcGjG2J0iKMTz8fQsL/3j9WFWb7KCIwpJZxEJfI4GyhWVVbtLi4/7ImplCwW1OUNxJOn78R6Gfng3e+Q2NSiNcBnwt6kGgclLrsHoJJ5n0wE16TEeWeZBewRep+fwhRD/ijN0MYtsqNZDhue6wVODwWCee63yhpsj+UMv7/+XPqxxMLvHRITaAepqZNBs6gZGl9BWNWaEGAjfkf2EqTNnG6ubH4SZOgS910IV5MjkkLIQx5jZvKWn1eEWCLe8ECNGav7fe9m6eruyLoYoKmRO7eW6B/i0g2nK9o0yVwlMLilEILllLwHXfUEfHyN59/HTmfzVk9K/jXQlSYZnl+i6CIwW+yDyhROuJUpA8BvYATX8vQbQseu1Jw3A11uPkdHQWQvupVUQkLDIzeZzgDkYHSYQ37XepEaI38IHg6hh9i8XJ9tGPoGYhsy9YMNBLBZKfEoogwIZU3hnoKT8YHMB07fNs3ku3SGmmiENJsF3HAsKU9EvoO9EJW9B02A5Iux0qZu8nl2IUSfbXmzdXflQ4qBd1gPhIQ2WNxc5ceLCP1or1KZtmdqmxSJun2n30KYitoZW/s4+vWyuEYpM1QUw4XvlxvGnzSBKpoenkGJx9yOHRPWoZD+xjpaPt2eS9c4EeKJ8QbIsWrPZs3VWFU13Cp+y+kJwUEwQ14TLKUHx+sUIehfJheRXTxW9WEUB2HlsY9ANHay5rPbk="),
+		},
+	)
+
 	return chart
 }
