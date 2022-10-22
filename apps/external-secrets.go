c3eedda05302e2a62c2fc66b5d95cf50a938652d
Patrick MARIE <pm@mkz.me>
2022-10-22 16:24:12 +0200

    Disable Secret-Store
    This should allow installing external-secrets

--- apps/external-secrets.go
+++ apps/external-secrets.go
@@ -1,7 +1,6 @@
 package apps
 
 import (
-	"git.mkz.me/mycroft/k8s-home/imports/secretstore_externalsecretsio"
 	k8s_helpers "git.mkz.me/mycroft/k8s-home/k8s-helpers"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
@@ -36,32 +35,32 @@ func NewExternalSecretsChart(scope constructs.Construct) cdk8s.Chart {
 		[]k8s_helpers.HelmReleaseConfigMap{},
 		nil,
 	)
-
-	secretstore_externalsecretsio.NewSecretStoreV1Beta1(
-		chart,
-		jsii.String("secret-store"),
-		&secretstore_externalsecretsio.SecretStoreV1Beta1Props{
-			Metadata: &cdk8s.ApiObjectMetadata{
-				Namespace: jsii.String(namespace),
-				Name:      jsii.String("vault-backend"),
-			},
-			Spec: &secretstore_externalsecretsio.SecretStoreV1Beta1Spec{
-				Provider: &secretstore_externalsecretsio.SecretStoreV1Beta1SpecProvider{
-					Vault: &secretstore_externalsecretsio.SecretStoreV1Beta1SpecProviderVault{
-						Server:  jsii.String("http://vault.vault:8200"),
-						Path:    jsii.String("secret/namespaces/"),
-						Version: secretstore_externalsecretsio.SecretStoreV1Beta1SpecProviderVaultVersion_V2,
-						Auth: &secretstore_externalsecretsio.SecretStoreV1Beta1SpecProviderVaultAuth{
-							Kubernetes: &secretstore_externalsecretsio.SecretStoreV1Beta1SpecProviderVaultAuthKubernetes{
-								MountPath: jsii.String("kubernetes"),
-								Role:      jsii.String("external-secrets"),
+	/*
+		secretstore_externalsecretsio.NewSecretStoreV1Beta1(
+			chart,
+			jsii.String("secret-store"),
+			&secretstore_externalsecretsio.SecretStoreV1Beta1Props{
+				Metadata: &cdk8s.ApiObjectMetadata{
+					Namespace: jsii.String(namespace),
+					Name:      jsii.String("vault-backend"),
+				},
+				Spec: &secretstore_externalsecretsio.SecretStoreV1Beta1Spec{
+					Provider: &secretstore_externalsecretsio.SecretStoreV1Beta1SpecProvider{
+						Vault: &secretstore_externalsecretsio.SecretStoreV1Beta1SpecProviderVault{
+							Server:  jsii.String("http://vault.vault:8200"),
+							Path:    jsii.String("secret/namespaces/"),
+							Version: secretstore_externalsecretsio.SecretStoreV1Beta1SpecProviderVaultVersion_V2,
+							Auth: &secretstore_externalsecretsio.SecretStoreV1Beta1SpecProviderVaultAuth{
+								Kubernetes: &secretstore_externalsecretsio.SecretStoreV1Beta1SpecProviderVaultAuthKubernetes{
+									MountPath: jsii.String("kubernetes"),
+									Role:      jsii.String("external-secrets"),
+								},
 							},
 						},
 					},
 				},
 			},
-		},
-	)
-
+		)
+	*/
 	return chart
 }
