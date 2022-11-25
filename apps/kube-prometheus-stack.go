a4c8e693b86b5c19f709cec307d3bb0dbc0b6da4
Patrick MARIE <pm@mkz.me>
2022-11-25 13:05:50 +0100

    Move Grafana secret to vault.
    Refs #2.

--- apps/kube-prometheus-stack.go
+++ apps/kube-prometheus-stack.go
@@ -20,21 +20,15 @@ func NewKubePrometheusStackChart(scope constructs.Construct) cdk8s.Chart {
 
 	k8s_helpers.NewNamespace(chart, namespace)
 
+	k8s_helpers.CreateSecretStore(chart, namespace)
+
 	k8s_helpers.CreateHelmRepository(
 		chart,
 		"prometheus-community",
 		"https://prometheus-community.github.io/helm-charts",
 	)
 
-	k8s_helpers.NewSealedSecret(
-		chart,
-		namespace,
-		"grafana-secret",
-		map[string]*string{
-			"password": jsii.String("AgA8Gj167Qfm8VlG1VkeU86rMF2LIotZmXjTHBw+t3bTY3zUjjIDJaJQKqYySTekDpGgA/+vR7erScMbVreyD+5rDYDe5XxOvrpK4Wa3C0QZ1GXhT9jg9T9DopQ7EwJQ7HUwmIOop3PIz7wa7SACabdDUioaxxhpI/gAAYWdA7jl25fXOm7AvdtxjmHaxzxY6AjrZ4VhJqVm8rcsHVF0J3Xw83HqproIM7JBg/oqavRrmLev6nbwUEE15sKXstfGqrXkhOeb/Gw8e8Ic+DzC1OuhfZChgohyLC/FtLx7TAymtb85DpXkxW3MaRU0bvna53dfA1tchTsi4SK/UMP/xQZgZemStTntoycEZ6XvKbLPjMdjuBza6ThZOfCEVSMBjqKKuaWNh3VnjN5s//vJxHw/WG4jeHwfntgo8dp1us69IQ/HV76tgKUABBBPoTGRtaN+jUSE4KTONcvIqtzc/JgM1EAxkYWoWXvLyNg3UOjPD4Jjr6xxxFvc8DC8HK85Xy91s8Yd1umcjYZ08VrkfSY3OVoS54S+dCy365cu5bZ4klAphs24DBj7Kmd16VQduqcIPwUSdRAUjuQg2qyPg+W78lysKZE/Gs8H203C4x1yE0K2tN1zVW4KQeGH276q+y9RWYDVQw9Op7vzZGMNkYjBH947EPt6uD/UHx5hdLsjLDvY5OMZhQaZ35dEyD9RxqM1ymjeUHEloUVp1dHNJ2nU"),
-			"user":     jsii.String("AgBYdmNCWw4ai42G50Fl1ydHNzKMTWfcpJGKqNvTaNUxe79EWQY0/fvUi5nWWzLNTOJpNHVgISPIdcW+WZT4Cu7ZPvmMzynEcqDMjaFWkOlbXtCrOQTfIOKEX1vAeGB6NWlLflpzqGykgiaUUXZ7xI7qvHv7ooUfLi/e7LTgZxDfqBC6DFrJ3R0YprS+PUoqHZTfkr15I4dnz2foA1A50XpLMweSqrmqcR1eHUSduqubplr5xIfUtdOhnuhCCe3H6PyjEoicCZ8cBPqbhHCAg2TwDifZrn9efJXkqT0WheZn17p2jPuUeCA+RrtOe8NNXAFm0+1zeowzVJCVCCone0b23SJ1Syre1muiTWEitD1Cs78xAn/2iAUWDUqgolrgV6LgeU8zwwt2013tbzd1ykzJv09tEjwzG2l49bv9+3L2lX18IwQgUGZToJhB9MQt/3z9h0sO6D2WelqLNpCA7GEywSCMIZLhszR9pTUIVPUZRxCQzgun51iySeGWiC1XLfF8WSPcG3YrmLWq5SWZI+xA4vRRHLtOyTSOubYAjF6gx2R/Rd4pMgIU4V/YnqrnlWDpnZz6oNQ6dHDM+vsRSRMQdJ7IJ4hdxvKMPzflvy1GptbqCijG1f/3bV13OWGhyWX/biHCIhcH4rxIQLbfuqs+BeFuWFdOUoSvB2d4HRM4wlEPoQNuCMhbtesfKFxN/uDpcGvhZQ=="),
-		},
-	)
+	k8s_helpers.CreateExternalSecret(chart, namespace, "grafana-secret")
 
 	k8s_helpers.CreateHelmRelease(
 		chart,
