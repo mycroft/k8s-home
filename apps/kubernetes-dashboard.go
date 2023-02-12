9b82ef77b7d0fe79248018dfc3130997c51a61b4
Patrick MARIE <pm@mkz.me>
2023-02-12 15:08:10 +0100

    kubernetes-dashboards: Multiple improvements
    Upgrading to latest helm chart;
    Protected ingress through SSO;
    read-only token less login;
    create a secret for admin token.

--- apps/kubernetes-dashboard.go
+++ apps/kubernetes-dashboard.go
@@ -31,7 +31,7 @@ func NewKubernetesDashboardChart(scope constructs.Construct) cdk8s.Chart {
 		"kubernetes-dashboard",
 		"kubernetes-dashboard", // chart name
 		"kubernetes-dashboard", // release name
-		"5.11.0",
+		"6.0.0",
 		map[string]string{},
 		[]k8s_helpers.HelmReleaseConfigMap{
 			k8s_helpers.CreateHelmValuesConfig(
@@ -77,5 +77,94 @@ func NewKubernetesDashboardChart(scope constructs.Construct) cdk8s.Chart {
 			},
 		},
 	)
+
+	// https://kubernetes.io/docs/concepts/configuration/secret/#service-account-token-secrets
+	// The token will be automatically filled in this secret:
+	k8s.NewKubeSecret(
+		chart,
+		jsii.String("sa-secret"),
+		&k8s.KubeSecretProps{
+			Metadata: &k8s.ObjectMeta{
+				Name:      jsii.String("secret-sa"),
+				Namespace: jsii.String(namespace),
+				Annotations: &map[string]*string{
+					"kubernetes.io/service-account.name": jsii.String("admin"),
+				},
+			},
+			Type: jsii.String("kubernetes.io/service-account-token"),
+		},
+	)
+
+	k8s.NewKubeClusterRole(
+		chart,
+		jsii.String("cluster-role-read-only"),
+		&k8s.KubeClusterRoleProps{
+			Metadata: &k8s.ObjectMeta{
+				Name: jsii.String("cluster-role-read-only"),
+			},
+			Rules: &[]*k8s.PolicyRule{
+				{
+					ApiGroups: &[]*string{
+						jsii.String("*"),
+					},
+					Verbs: &[]*string{
+						jsii.String("get"),
+						jsii.String("list"),
+						jsii.String("watch"),
+					},
+					Resources: &[]*string{
+						jsii.String("namespaces"),
+						jsii.String("events"),
+						jsii.String("pods"),
+						jsii.String("cronjobs"),
+						jsii.String("jobs"),
+						jsii.String("replicasets"),
+						jsii.String("deployments"),
+						jsii.String("daemonsets"),
+						jsii.String("replicationcontrollers"),
+						jsii.String("statefulsets"),
+						jsii.String("ingresses"),
+						jsii.String("ingressesclasses"),
+						jsii.String("services"),
+						jsii.String("configmaps"),
+						jsii.String("persistentvolumeclaims"),
+						jsii.String("storageclasses"),
+						jsii.String("clusterrolebindings"),
+						jsii.String("clusterroles"),
+						jsii.String("networkpolicies"),
+						jsii.String("nodes"),
+						jsii.String("persistentvolumes"),
+						jsii.String("rolebindings"),
+						jsii.String("roles"),
+						jsii.String("serviceaccounts"),
+						jsii.String("customresourcedefinitions"),
+					},
+				},
+			},
+		},
+	)
+
+	k8s.NewKubeClusterRoleBinding(
+		chart,
+		jsii.String("cluster-role-binding-kubernetes-dashboard"),
+		&k8s.KubeClusterRoleBindingProps{
+			Metadata: &k8s.ObjectMeta{
+				Name: jsii.String("kubernetes-dashboard"),
+			},
+			RoleRef: &k8s.RoleRef{
+				ApiGroup: jsii.String("rbac.authorization.k8s.io"),
+				Kind:     jsii.String("ClusterRole"),
+				Name:     jsii.String("cluster-role-read-only"),
+			},
+			Subjects: &[]*k8s.Subject{
+				{
+					Kind:      jsii.String("ServiceAccount"),
+					Name:      jsii.String("kubernetes-dashboard"),
+					Namespace: jsii.String(namespace),
+				},
+			},
+		},
+	)
+
 	return chart
 }
