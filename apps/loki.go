39e47b8d1bafff90d3efbcdfa76b61f393773ec6
Patrick MARIE <pm@mkz.me>
2023-05-19 21:57:57 +0200

    Upgrade loki to 5.5.2

--- apps/loki.go
+++ apps/loki.go
@@ -36,7 +36,7 @@ func NewLokiChart(scope constructs.Construct) cdk8s.Chart {
 		repositoryName,
 		chartName,
 		releaseName,
-		"5.5.1",
+		"5.5.2",
 		map[string]string{},
 		[]k8s_helpers.HelmReleaseConfigMap{
 			k8s_helpers.CreateHelmValuesConfig(
