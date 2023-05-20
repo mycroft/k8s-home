39ea64a24ccfeb3b0ca6cf51f4176cec26eddbe0
Patrick MARIE <pm@mkz.me>
2023-05-20 16:43:47 +0200

    Upgrade kube-prometheus-stack to 45.29.0

--- apps/kube-prometheus-stack.go
+++ apps/kube-prometheus-stack.go
@@ -43,7 +43,7 @@ func NewKubePrometheusStackChart(scope constructs.Construct) cdk8s.Chart {
 		repositoryName, // repoName; must be in flux-system
 		chartName,      // chart name
 		releaseName,    // release name
-		"45.28.1",
+		"45.29.0",
 		map[string]string{},
 		[]k8s_helpers.HelmReleaseConfigMap{
 			k8s_helpers.CreateHelmValuesConfig(
