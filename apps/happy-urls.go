7278b2fcf651acbddedc1ba8c216a7d863a9670e
Patrick MARIE <pm@mkz.me>
2023-08-18 11:41:15 +0200

    Upgrading redis in happy-urls

--- apps/happy-urls.go
+++ apps/happy-urls.go
@@ -37,7 +37,7 @@ func NewHappyUrlsChart(scope constructs.Construct) cdk8s.Chart {
 		chart,
 		namespace,
 		"redis",
-		"redis:7.0.9",
+		"redis:7.2.0",
 		6379,
 		redisLabels,
 		[]*k8s.EnvVar{},
