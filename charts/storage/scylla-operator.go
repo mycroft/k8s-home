a27728a6fa32b2cc9fc13c8f8cad8277f6ce7743
Patrick Marie <pm@mkz.me>
2024-05-15 20:19:34 +0200

    docs(scylla): link operator CRDs

--- charts/storage/scylla-operator.go
+++ charts/storage/scylla-operator.go
@@ -7,6 +7,15 @@ import (
 	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
 )
 
+// CRDs are:
+//
+// https://raw.githubusercontent.com/scylladb/scylla-operator/master/pkg/api/scylla/v1alpha1/scylla.scylladb.com_scylladbmonitorings.yaml
+// https://raw.githubusercontent.com/scylladb/scylla-operator/master/pkg/api/scylla/v1alpha1/scylla.scylladb.com_nodeconfigs.yaml
+// https://raw.githubusercontent.com/scylladb/scylla-operator/master/pkg/api/scylla/v1alpha1/scylla.scylladb.com_scyllaoperatorconfigs.yaml
+// https://raw.githubusercontent.com/scylladb/scylla-operator/master/pkg/api/scylla/v1/scylla.scylladb.com_scyllaclusters.yaml
+//
+// TODO: Automatize CRDs checks/updates
+
 func NewScyllaOperatorChart(scope constructs.Construct) cdk8s.Chart {
 	namespace := "scylla-operator"
 
