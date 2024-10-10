75127fd7465662a22b881681ac2142e8cded0c0e
Patrick Marie <pm@mkz.me>
2024-10-10 11:09:47 +0200

    feat: adding temporal upgrade script

---
+++ contrib/upgrade-temporal.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+DRY=
+
+NS=temporal
+
+ADMIN_POD=$(kubectl get pods -l app.kubernetes.io/component=admintools -o name)
+
+POSTGRES_SEEDS=postgres-instance.postgres
+DB_PORT=5432
+
+DBNAME=temporal
+POSTGRES_USER=$(kubectl get secret -o yaml postgresql -o json | jq -r .data.username | base64 -d)
+POSTGRES_PWD=$(kubectl get secret -o yaml postgresql -o json | jq -r .data.password | base64 -d)
+kubectl exec -ti ${ADMIN_POD} -- ${DRY} temporal-sql-tool --plugin postgres12 --ep "${POSTGRES_SEEDS}" -u "${POSTGRES_USER}" -pw "${POSTGRES_PWD}" -p "${DB_PORT}" --db "${DBNAME}" update-schema -d "/etc/temporal/schema/postgresql/v12/temporal/versioned"
+
+DBNAME=temporal_visibility
+POSTGRES_USER=$(kubectl get secret -o yaml postgresql_visibility -o json | jq -r .data.username | base64 -d)
+POSTGRES_PWD=$(kubectl get secret -o yaml postgresql_visibility -o json | jq -r .data.password | base64 -d)
+
+kubectl exec -ti ${ADMIN_POD} -- ${DRY} temporal-sql-tool --plugin postgres12 --ep "${POSTGRES_SEEDS}" -u "${POSTGRES_USER}" -pw "${POSTGRES_PWD}" -p "${DB_PORT}" --db "${DBNAME}" update-schema -d "/etc/temporal/schema/postgresql/v12/visibility/versioned"
