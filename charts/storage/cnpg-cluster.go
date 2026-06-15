0b2a45d248bab66b92f8e0b30e09748312df9fad
Patrick MARIE <pm@mkz.me>
2026-06-15 11:59:31 +0200

    Add zipline database and managed role to CNPG cluster
    Imports the cnpg Database CRD and creates a managed "zipline" role
    plus a Database resource so the zipline app can use its own
    database/owner on the shared cnpg-cluster postgres instance.

--- charts/storage/cnpg-cluster.go
+++ charts/storage/cnpg-cluster.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"git.mkz.me/mycroft/k8s-home/imports/cnpg_cluster_postgresqlcnpgio"
+	database "git.mkz.me/mycroft/k8s-home/imports/cnpg_database_postgresqlcnpgio"
 	"git.mkz.me/mycroft/k8s-home/internal/kubehelpers"
 	"github.com/aws/jsii-runtime-go"
 	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
@@ -13,7 +14,19 @@ func NewCNPGCluster(builder *kubehelpers.Builder) *kubehelpers.Chart {
 	chart := builder.NewChart(namespace)
 	chart.NewNamespace(namespace)
 
-	cnpg_cluster_postgresqlcnpgio.NewCluster(
+	managedRoles := []*cnpg_cluster_postgresqlcnpgio.ClusterSpecManagedRoles{
+		{
+			Name:            jsii.String("zipline"),
+			Ensure:          cnpg_cluster_postgresqlcnpgio.ClusterSpecManagedRolesEnsure_PRESENT,
+			Createdb:        jsii.Bool(false),
+			Createrole:      jsii.Bool(false),
+			DisablePassword: jsii.Bool(false),
+			Login:           jsii.Bool(true),
+			Superuser:       jsii.Bool(false),
+		},
+	}
+
+	cluster := cnpg_cluster_postgresqlcnpgio.NewCluster(
 		chart.Cdk8sChart,
 		jsii.String("cnpg-cluster"),
 		&cnpg_cluster_postgresqlcnpgio.ClusterProps{
@@ -28,12 +41,33 @@ func NewCNPGCluster(builder *kubehelpers.Builder) *kubehelpers.Chart {
 						Database: jsii.String("postgres"),
 					},
 				},
+				Managed: &cnpg_cluster_postgresqlcnpgio.ClusterSpecManaged{
+					Roles: &managedRoles,
+				},
 				EnableSuperuserAccess: jsii.Bool(true),
 				Instances:             jsii.Number(2),
 				Storage: &cnpg_cluster_postgresqlcnpgio.ClusterSpecStorage{
 					StorageClass: jsii.String("longhorn-crypto-global"),
 					Size:         jsii.String("64Gi"),
 				},
+			},
+		},
+	)
+
+	database.NewDatabase(
+		chart.Cdk8sChart,
+		jsii.String("zipline"),
+		&database.DatabaseProps{
+			Metadata: &cdk8s.ApiObjectMetadata{
+				Namespace: jsii.String(namespace),
+				Name:      jsii.Sprintf("postgres-zipline"),
+			},
+			Spec: &database.DatabaseSpec{
+				Cluster: &database.DatabaseSpecCluster{
+					Name: cluster.Name(),
+				},
+				Name:  jsii.String("zipline"),
+				Owner: jsii.String("zipline"),
 			},
 		},
 	)
