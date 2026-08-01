9add12fa11c0ecc9fc7a7cffa1a7e9c5f0b042c6
Patrick MARIE <pm@mkz.me>
2026-08-01 10:39:43 +0200

    feat(zipline): prepare cnpg cutover
    - Materialize the staged CNPG credentials in Zipline's namespace
    - Point Zipline at CNPG while retaining legacy secrets for rollback

--- charts/apps/zipline.go
+++ charts/apps/zipline.go
@@ -23,6 +23,7 @@ func NewZiplineChart(builder *kubehelpers.Builder) *kubehelpers.Chart {
 	kubehelpers.CreateSecretStore(chart.Cdk8sChart, namespace)
 	kubehelpers.CreateExternalSecret(chart.Cdk8sChart, namespace, "postgresql")
 	kubehelpers.CreateExternalSecret(chart.Cdk8sChart, namespace, "postgresql-v4")
+	kubehelpers.CreateExternalSecret(chart.Cdk8sChart, namespace, "postgresql-cnpg")
 	kubehelpers.CreateExternalSecret(chart.Cdk8sChart, namespace, "core")
 
 	env := []*k8s.EnvVar{
@@ -40,7 +41,7 @@ func NewZiplineChart(builder *kubehelpers.Builder) *kubehelpers.Chart {
 			ValueFrom: &k8s.EnvVarSource{
 				SecretKeyRef: &k8s.SecretKeySelector{
 					Key:  jsii.String("url"),
-					Name: jsii.String("postgresql-v4"),
+					Name: jsii.String("postgresql-cnpg"),
 				},
 			},
 		},
